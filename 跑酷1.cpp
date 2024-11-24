3f5ef7f8f07f1ab91b21bbeaf9000077991bfeea
Han24666 <14883095+han24666@user.noreply.gitee.com>
2024-11-24 08:50:06 +0000

    上传v0.0.1版本的源代码
    Signed-off-by: Han24666 <14883095+han24666@user.noreply.gitee.com>

---
+++ 跑酷1.cpp
@@ -0,0 +1,31 @@
+﻿#include<bits/stdc++.h>
+#include<easyx.h>
+#include<conio.h>
+#include"game.h"
+using namespace std;
+int main(){
+	initgraph(640,320);
+	setbkcolor(WHITE);
+	cleardevice();
+	int x=300,y=10;
+	while(1){
+		down(x,y);
+		drawperson(x,y);
+		drawfloor();
+		char ch=_getch();
+		switch(ch){
+		    case 'w':
+				jump(x,y);
+				break;
+			case 'a':
+				y-=5;
+				break;
+			case 'd':
+				y+=5;
+				break;
+			default:
+				break;
+		}
+	}
+	return 0;
+}
